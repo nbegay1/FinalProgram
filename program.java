81a5ea27b38b9b354886b5e862fa0db41abf04ba
Nicholette B <90649706+nbegay1@users.noreply.github.com>
2021-12-07 13:09:03 -0700

    Create program.java

---
+++ program.java
@@ -0,0 +1,22 @@
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class dialougebox {
+
+   public static void main(String[] args) {
+   int favoritcolor;
+   JFrame topFrame = null;
+   JLabel colorLabel = null;
+   JFrame appFrame = new JFrame();
+   appFrame.setTitle("Favorite Color");
+   appFrame.setSize(500, 300);
+   appFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ 
+   appFrame.setVisible(true);
+ 
+   }
+}
